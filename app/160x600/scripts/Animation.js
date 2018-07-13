279faf30f9f55d5d51c22a06891e6ffe3a5f234a
tim.hennessey <Tim.Hennessey@deutschinc.com>
2018-07-13 14:12:50 -0700

    Resizes

---
+++ app/160x600/scripts/Animation.js
@@ -0,0 +1,56 @@
+var app = app || {};
+
+app.Animation = (function () {
+
+    var t = TweenMax;
+    var tl1 = new TimelineMax();
+    var txt1 = document.getElementById('txt1');
+    var hoodie = document.getElementById('hoodie');
+    var jersey = document.getElementById('jersey');
+    var pants = document.getElementById('pants');
+    var shoes = document.getElementById('shoes');
+
+    var cta = document.getElementById('cta');
+    var cta_txt = document.getElementById('cta_txt');
+    var cta_txt_ovr = document.getElementById('cta_txt_ovr');
+    var buttonExit = document.getElementById('button-exit');
+
+	// --------------------------------------------------------------------------------------
+	function initialize() {
+		t.set(banner, {opacity:1});
+
+        buttonExit.addEventListener('mouseover', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#ffffff"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:1});
+        });
+
+        buttonExit.addEventListener('mouseout', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#cc0000"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:0});
+        });
+
+	}
+	// --------------------------------------------------------------------------------------
+	function start() {
+        tl1.from(jersey, 1, {x:-300, ease: Cubic.easeOut})
+            .from(pants, 1, {x:-300, ease: Cubic.easeOut},"-=.75")
+            .from(shoes, 1, {x:-300, ease: Cubic.easeOut},"-=.8")
+            .from(hoodie, 1, {x:-300, ease: Cubic.easeOut},"-=.8")
+            .from(txt1, 1, {y: -177, ease: Cubic.easeInOut})
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
